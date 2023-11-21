75af976379dcb80cbc429b248872df809839985f
brandonom <brandonomyers1@gmail.com>
2023-11-20 21:31:45 -0500

    deplying

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql`
+
+export const login_user = gql`
   mutation loginUser($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
