cb09ed053d9b36840e4e54cf1455bcb3b0b8f1da
brandonom <brandonomyers1@gmail.com>
2023-11-20 19:05:46 -0500

    refactored server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,37 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schema");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const isProd = process.env.NODE_ENV === "production";
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 
-app.use(routes);
+if (isProd) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
 
-db.once('open', () => {
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startServer();
+
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
